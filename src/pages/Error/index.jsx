7fee026b01bedc5ac7ebe0aacf7865f01f79ee3f
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-04-01 01:37:31 +0600

    custom  404 page created

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,21 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../assets/images/404.png'
 
 const ErrorPage = () => {
     return (
-        <div>
-            This is error page
-        </div>
+        <>
+            <div className='bg-base-100 py-20 px-5'>
+                <img className='mx-auto' src={notFound} alt="" />
+                <h2 className='text-5xl font-extrabold text-neutral text-center'>Oops! That page can't be found.</h2>
+                <p className='text-[#74777c] text-center py-8 text-lg'>Unfortunately, something went wrong and this page does not exist. Try <br /> using the search or return to the previous page.</p>
+                <div className='flex justify-center'>
+
+                    <Link to='/'><Button sx={{ borderRadius: '45px', background: '#ff1e00', padding: '5px 20px', '&:hover': { backgroundColor: '#011B39' } }} variant="contained" >Back to Home</Button></Link>
+                </div>
+            </div>
+        </>
     );
 };
 
